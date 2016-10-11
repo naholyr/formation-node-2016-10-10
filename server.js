07c297c1a543ada78998dd9fa546f6aa67444fb8
Nicolas Chambrier <naholyr@gmail.com>
2016-10-11 15:41:02 +0200

    express: quiz routes

--- server.js
+++ server.js
@@ -4,8 +4,59 @@ const http = require('http')
 const express = require('express')
 const chalk = require('chalk')
 const config = require('config')
+const questions = require('./lib/routes/questions')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+
+// Disable X-Powered-By header
+app.set('x-powered-by', false)
+
+
+/*
+ bodyParser.json() =~= function (req, res, next) {
+    if (req.method === 'POST' && req.headers['content-type'] === 'application/json') {
+      req.on('data') → bufferiser
+      req.on('end') → parser + req.body = résultat du parsing + next()
+    } else {
+      next()
+    }
+ }
+*/
+app.use(bodyParser.json())
+
+/*
+function restricted (role) {
+  return function (req, res, next) {
+    if (!utilisateurARole(role)) {
+      res.status(403).end()
+    } else {
+      next()
+    }
+  }
+}
+*/
+
+
+// Routing
+// app.méthode(url, handler)
+
+app.get('/', /*restricted('admin'),*/ (req, res) => {
+  res.send({ hello: 'world' })
+})
+
+app.get('/param/:variable([0-9]+)', (req, res) => {
+  res.send({ variable: req.params.variable })
+})
+
+app.get('/query', (req, res) => {
+  res.send({ variable: req.query.variable })
+})
+
+app.get('/next-question', questions.next)
+app.post('/answer-question', questions.answer)
+
 
 const server = http.createServer(app)
 
